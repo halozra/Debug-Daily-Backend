3521dec3ace2e882123162c9acca9836864b6c8e
halozra <revvv03@gmail.com>
2025-02-14 00:56:10 +0700

    fix lmao

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.use(
     allowedHeaders: "Content-Type,Authorization",
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://debug-daily-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
