4cc6f892357e85e4246c8ba9e6f03e9a8cf7bc35
halozra <revvv03@gmail.com>
2025-02-14 02:37:29 +0700

    fix: enable CORS for multiple frontend origins

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const allowedOrigins = ['http://localhost:3000', 'http://192.168.225.175:3000',`${process.env.FRONTEND_URL}`];
+
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://debug-daily-frontend.vercel.app/", // Ganti dengan domain frontend-mu
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true); // Mengizinkan origin yang ada di allowedOrigins
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   })
 );
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://debug-daily-frontend.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 // Routes
 app.use("/api/auth", authRoutes);
