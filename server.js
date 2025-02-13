339d4a932e64a2d223ca19226350d582cee7cc96
halozra <revvv03@gmail.com>
2025-02-14 00:47:52 +0700

    add cors https

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://debug-daily-frontend.vercel.app/", // Ganti dengan domain frontend-mu
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
